011091844763b9d73ebd02dd4cfed61ab903bfca
Sanan296 <sanantahir29@gmail.com>
2025-10-01 16:30:57 +0500

    Added StudentCard component with styled attendance and updated related files

--- studentloginpage/src/App.jsx
+++ studentloginpage/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
-import StudentLogin from './components/loginpage.jsx'
+import StudentLogin from './components/loginpage.jsx';
+import StudentCard from './components/studentcard.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <StudentCard/>  
     <StudentLogin/>
     </>
   )
--- studentloginpage/src/components/loginpage.jsx
+++ studentloginpage/src/components/loginpage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./loginpage.css"; // keep your CSS here
+import "./loginpage.css"; 
 
 function StudentLogin() {
   return (
